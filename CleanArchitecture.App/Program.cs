ae7a9778b946aeb3e0e35f49799237d27b741a3c
Pierre Belin <contact@pierrebelin.fr>
2023-04-29 16:15:10 +0200

    feat: add health checks

--- CleanArchitecture.App/Program.cs
+++ CleanArchitecture.App/Program.cs
@@ -3,12 +3,15 @@ using System.Reflection;
 using CleanArchitecture.App.Controllers;
 using CleanArchitecture.Application.Customers.Commands;
 using CleanArchitecture.Application.Customers.Queries;
+using CleanArchitecture.Application.HealthChecks;
 using CleanArchitecture.Application.Products.Commands;
 using CleanArchitecture.Application.Products.Queries;
 using CleanArchitecture.Domain.Persistence;
 using CleanArchitecture.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using MassTransit;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using CleanArchitecture.App.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +36,9 @@ builder.Services.AddDbContext<EfDbContext>();
 builder.Services.AddTransient<DbContext, EfDbContext>();
 builder.Services.AddTransient<IDataServiceFactory, DataServiceFactory>();
 
+builder.Services.AddHealthChecks()
+    .AddCheck<PersistenceHealthCheck>(nameof(PersistenceHealthCheck));
+
 SqlMapper.AddTypeHandler(new MySqlGuidTypeHandler());
 SqlMapper.RemoveTypeMap(typeof(Guid));
 SqlMapper.RemoveTypeMap(typeof(Guid?));
@@ -48,6 +54,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = HealthCheckExtensions.WriteHealthCheckResponse
+});
 
 app.UseHttpsRedirection();
 
---
+++ CleanArchitecture.Application/HealthChecks/PersistenceHealthCheck.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchitecture.Domain.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CleanArchitecture.Application.HealthChecks;
+
+public class PersistenceHealthCheck : IHealthCheck
+{
+
+    private readonly DbContext _dbContext;
+
+    public PersistenceHealthCheck(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        if (_dbContext.Database.CanConnect())
+        {
+            return Task.FromResult(
+                HealthCheckResult.Healthy("Database is healthy"));
+        }
+
+        return Task.FromResult(
+            new HealthCheckResult(
+                context.Registration.FailureStatus, "Database is failing"));
+    }
+}
